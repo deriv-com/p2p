85402e755e0130e51a3f277f1e161bdaccaa2eb5
Farrah Mae Ochoa <farrah@deriv.com>
2024-04-23 20:18:32 +0400

    fix: svg config

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,5 +1,5 @@
-export const BUY_SELL_URL = 'buy-sell';
-export const ORDERS_URL = 'orders';
-export const MY_ADS_URL = 'my-ads';
-export const MY_PROFILE_URL = 'my-profile';
-export const ADVERTISER_URL = 'advertiser';
+export const BUY_SELL_URL = '/buy-sell';
+export const ORDERS_URL = '/orders';
+export const MY_ADS_URL = '/my-ads';
+export const MY_PROFILE_URL = '/my-profile';
+export const ADVERTISER_URL = '/advertiser';
