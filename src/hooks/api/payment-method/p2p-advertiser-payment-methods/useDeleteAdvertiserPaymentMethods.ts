df306d5ef0fcd109b5a2f45bdbb5db3a29704ca6
Nada <nada.ajinas@deriv.com>
2024-04-26 10:52:27 +0400

    fix: build error

--- src/hooks/api/payment-method/p2p-advertiser-payment-methods/useDeleteAdvertiserPaymentMethods.ts
+++ src/hooks/api/payment-method/p2p-advertiser-payment-methods/useDeleteAdvertiserPaymentMethods.ts
@@ -1,14 +1,14 @@
 import { useCallback } from 'react';
-import { useP2pAdvertiserPaymentMethods } from '@deriv-com/api-hooks';
+import { useP2PAdvertiserPaymentMethods } from '@deriv-com/api-hooks';
 import useInvalidateQuery from '../../useInvalidateQuery';
 /** A custom hook that sends a request to delete an existing p2p advertiser payment method. */
 const useDeleteAdvertiserPaymentMethods = () => {
     const invalidate = useInvalidateQuery();
-    const { data, mutate, ...rest } = useP2pAdvertiserPaymentMethods({
+    const { data, mutate, ...rest } = useP2PAdvertiserPaymentMethods({
         onSuccess: () => invalidate('p2p_advertiser_payment_methods'),
     });
 
-    const deletePaymentMethod = useCallback((id: number) => mutate({ payload: { delete: [id] } }), [mutate]);
+    const deletePaymentMethod = useCallback((id: number) => mutate({ delete: [id] }), [mutate]);
 
     return {
         data,
