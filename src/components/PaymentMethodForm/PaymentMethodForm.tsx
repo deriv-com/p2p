f9b450eb08c38a479aa8c40d1e51e4b01d7b7637
Nada <nada.ajinas@deriv.com>
2024-05-03 09:48:22 +0400

    fix: cancel modal issue fixed

--- src/components/PaymentMethodForm/PaymentMethodForm.tsx
+++ src/components/PaymentMethodForm/PaymentMethodForm.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { TAdvertiserPaymentMethod, TFormState, TSelectedPaymentMethod } from 'types';
 import { PageReturn, PaymentMethodField, PaymentMethodsFormFooter } from '@/components';
-import { api, useModalManager } from '@/hooks';
+import { api } from '@/hooks';
 import { Button, Modal, useDevice } from '@deriv-com/ui';
 import { PaymentMethodFormAutocomplete } from './PaymentMethodFormAutocomplete';
 import { PaymentMethodFormModalRenderer } from './PaymentMethodFormModalRenderer';
@@ -35,7 +35,7 @@ const PaymentMethodForm = ({
         handleSubmit,
         reset,
     } = useForm({ mode: 'all' });
-    const { hideModal, isModalOpenFor, showModal } = useModalManager();
+    const [isError, setIsError] = useState(false);
     const { actionType, selectedPaymentMethod, title = '' } = rest.formState;
     const { data: availablePaymentMethods } = api.paymentMethods.useGet();
     const { create, error: createError, isSuccess: isCreateSuccessful } = api.advertiserPaymentMethods.useCreate();
@@ -65,7 +65,7 @@ const PaymentMethodForm = ({
     }, [availablePaymentMethods]);
     const handleGoBack = () => {
         if (isDirty) {
-            showModal('PaymentMethodFormErrorModal', { shouldStackModals: true });
+            setIsError(true);
         } else {
             onResetFormState();
             onRequestClose?.();
@@ -74,96 +74,110 @@ const PaymentMethodForm = ({
 
     if (displayModal) {
         return (
-            <Modal
-                ariaHideApp={false}
-                className='payment-method-form__modal'
-                isOpen={displayModal}
-                onRequestClose={handleGoBack}
-                shouldCloseOnOverlayClick={false}
-            >
-                <form
-                    className='payment-method-form__form'
-                    onSubmit={event => {
-                        event.preventDefault(); // Prevents create edit ad form submission
-                        event.stopPropagation();
-                        handleSubmit(data => {
-                            const hasData = Object.keys(data).length > 0;
-                            if (actionType === 'ADD' && hasData) {
-                                create({
-                                    ...data,
-                                    method: String((selectedPaymentMethod as TAdvertiserPaymentMethod)?.method),
-                                });
-                            } else if (actionType === 'EDIT' && hasData) {
-                                update(String(selectedPaymentMethod?.id), {
-                                    ...data,
-                                });
-                            }
-                        })(event);
-                    }}
+            <>
+                <Modal
+                    ariaHideApp={false}
+                    className='payment-method-form__modal'
+                    isOpen={displayModal}
+                    onRequestClose={handleGoBack}
+                    shouldCloseOnOverlayClick={false}
                 >
-                    <Modal.Header hideBorder onRequestClose={handleGoBack}>
-                        <PageReturn
-                            className='mb-0'
-                            hasBorder={isMobile}
-                            onClick={handleGoBack}
-                            pageTitle='Add payment method'
-                            size={isMobile ? 'lg' : 'md'}
-                            weight='bold'
-                        />
-                    </Modal.Header>
-                    <Modal.Body className='payment-method-form__modal__body'>
-                        <div className='payment-method-form__fields'>
-                            <div className='payment-method-form__field-wrapper'>
-                                <PaymentMethodFormAutocomplete
-                                    actionType={actionType}
-                                    availablePaymentMethods={availablePaymentMethods}
-                                    availablePaymentMethodsList={availablePaymentMethodsList}
-                                    onAdd={onAdd}
-                                    reset={reset}
-                                    selectedPaymentMethod={selectedPaymentMethod}
-                                />
-                            </div>
-                            {Object.keys(selectedPaymentMethod?.fields || {})?.map(field => {
-                                const paymentMethodField =
-                                    (selectedPaymentMethod?.fields as TAdvertiserPaymentMethod['fields'])?.[field] ??
-                                    {};
-                                return (
-                                    <PaymentMethodField
-                                        control={control}
-                                        defaultValue={paymentMethodField?.value ?? ''}
-                                        displayName={paymentMethodField?.display_name ?? ''}
-                                        field={field}
-                                        key={field}
-                                        required={!!paymentMethodField?.required}
-                                    />
-                                );
-                            })}
-                        </div>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        {selectedPaymentMethod ? (
-                            <PaymentMethodsFormFooter
-                                actionType={actionType}
-                                handleGoBack={handleGoBack}
-                                isDirty={isDirty}
-                                isSubmitting={isSubmitting}
-                                isValid={isValid}
+                    <form
+                        className='payment-method-form__form'
+                        onSubmit={event => {
+                            event.preventDefault(); // Prevents create edit ad form submission
+                            event.stopPropagation();
+                            handleSubmit(data => {
+                                const hasData = Object.keys(data).length > 0;
+                                if (actionType === 'ADD' && hasData) {
+                                    create({
+                                        ...data,
+                                        method: String((selectedPaymentMethod as TAdvertiserPaymentMethod)?.method),
+                                    });
+                                } else if (actionType === 'EDIT' && hasData) {
+                                    update(String(selectedPaymentMethod?.id), {
+                                        ...data,
+                                    });
+                                }
+                            })(event);
+                        }}
+                    >
+                        <Modal.Header hideBorder onRequestClose={handleGoBack}>
+                            <PageReturn
+                                className='mb-0'
+                                hasBorder={isMobile}
+                                onClick={handleGoBack}
+                                pageTitle='Add payment method'
+                                size={isMobile ? 'lg' : 'md'}
+                                weight='bold'
                             />
-                        ) : (
-                            <Button
-                                className='border-2'
-                                color='black'
-                                onClick={onRequestClose}
-                                size='lg'
-                                textSize='sm'
-                                variant='outlined'
-                            >
-                                Cancel
-                            </Button>
-                        )}
-                    </Modal.Footer>
-                </form>
-            </Modal>
+                        </Modal.Header>
+                        <Modal.Body className='payment-method-form__modal__body'>
+                            <div className='payment-method-form__fields'>
+                                <div className='payment-method-form__field-wrapper'>
+                                    <PaymentMethodFormAutocomplete
+                                        actionType={actionType}
+                                        availablePaymentMethods={availablePaymentMethods}
+                                        availablePaymentMethodsList={availablePaymentMethodsList}
+                                        onAdd={onAdd}
+                                        reset={reset}
+                                        selectedPaymentMethod={selectedPaymentMethod}
+                                    />
+                                </div>
+                                {Object.keys(selectedPaymentMethod?.fields || {})?.map(field => {
+                                    const paymentMethodField =
+                                        (selectedPaymentMethod?.fields as TAdvertiserPaymentMethod['fields'])?.[
+                                            field
+                                        ] ?? {};
+                                    return (
+                                        <PaymentMethodField
+                                            control={control}
+                                            defaultValue={paymentMethodField?.value ?? ''}
+                                            displayName={paymentMethodField?.display_name ?? ''}
+                                            field={field}
+                                            key={field}
+                                            required={!!paymentMethodField?.required}
+                                        />
+                                    );
+                                })}
+                            </div>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            {selectedPaymentMethod ? (
+                                <PaymentMethodsFormFooter
+                                    actionType={actionType}
+                                    handleGoBack={handleGoBack}
+                                    isDirty={isDirty}
+                                    isSubmitting={isSubmitting}
+                                    isValid={isValid}
+                                />
+                            ) : (
+                                <Button
+                                    className='border-2'
+                                    color='black'
+                                    onClick={onRequestClose}
+                                    size='lg'
+                                    textSize='sm'
+                                    variant='outlined'
+                                >
+                                    Cancel
+                                </Button>
+                            )}
+                        </Modal.Footer>
+                    </form>
+                </Modal>
+                {isError && (
+                    <PaymentMethodFormModalRenderer
+                        actionType={actionType}
+                        createError={createError}
+                        isCreateSuccessful={isCreateSuccessful}
+                        isUpdateSuccessful={isUpdateSuccessful}
+                        onResetFormState={onResetFormState}
+                        setIsError={setIsError}
+                        updateError={updateError}
+                    />
+                )}
+            </>
         );
     }
 
@@ -229,15 +243,14 @@ const PaymentMethodForm = ({
                     />
                 )}
             </form>
-            {isModalOpenFor('PaymentMethodFormErrorModal') && (
+            {isError && (
                 <PaymentMethodFormModalRenderer
                     actionType={actionType}
                     createError={createError}
                     isCreateSuccessful={isCreateSuccessful}
-                    isModalOpen={!!isModalOpenFor('PaymentMethodFormErrorModal')}
                     isUpdateSuccessful={isUpdateSuccessful}
-                    onRequestClose={hideModal}
                     onResetFormState={onResetFormState}
+                    setIsError={setIsError}
                     updateError={updateError}
                 />
             )}
