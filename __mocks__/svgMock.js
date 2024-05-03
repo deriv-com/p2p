9d1c0fd08d431fcaf86a84f90170a98a9a1d2429
adrienne-rio <adrienne@deriv.com>
2024-05-03 11:06:51 +0800

    chore: fixed a jest issue where svgs cant be imported

---
+++ __mocks__/svgMock.js
@@ -0,0 +1,4 @@
+// NOTE: This will mock SVG imports with ...svg?react
+export default 'SvgrURL';
+// NOTE: This will mock SVG imports with { ReactComponent as SomeIcon } from ...svg
+export const ReactComponent = 'div';
