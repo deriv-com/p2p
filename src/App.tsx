30900e3bb09498a269e395c032071212a425c0be
Nada <nada.ajinas@deriv.com>
2024-05-08 14:47:38 +0400

    fix: hardcoded branch name and project name

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,9 @@ import { Header } from '@/components';
 import { initializeI18n, TranslationProvider } from '@deriv-com/translations';
 import AppContent from './routes/AppContent';
 
+//TODO: replace with ${process.env.VITE_PROJECT_NAME}/${process.env.VITE_CROWDIN_BRANCH_NAME}
 const i18nInstance = initializeI18n({
-    cdnUrl: `https://pub-5ce11fcb15f34c0a9ce8ba7086d16e6a.r2.dev/${process.env.VITE_PROJECT_NAME}/${process.env.VITE_CROWDIN_BRANCH_NAME}`,
+    cdnUrl: `https://pub-5ce11fcb15f34c0a9ce8ba7086d16e6a.r2.dev/p2p/DP2P`,
 });
 
 const App = () => {
