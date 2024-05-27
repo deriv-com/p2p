cea29884b1322c05a1ad5d6396c0bc25bec856f6
shayan khaleghparast <iman@regentmarkets.com>
2024-05-27 14:31:05 +0800

    fix: fixed test failing issue

--- src/components/AppHeader/__tests__/AppHeader.spec.tsx
+++ src/components/AppHeader/__tests__/AppHeader.spec.tsx
@@ -1,13 +1,68 @@
 import { ReactNode } from 'react';
 import { useAuthData } from '@deriv-com/api-hooks';
-import { URLUtils } from '@deriv-com/utils';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import AppHeader from '../AppHeader';
 
 const mockUseAuthData = useAuthData as jest.Mock;
 jest.mock('@deriv-com/api-hooks', () => ({
+    useAccountList: jest.fn(() => ({
+        data: [
+            {
+                account_category: 'trading',
+                account_type: 'binary',
+                broker: 'CR',
+                currency: 'USD',
+                loginid: 'CR90000383',
+            },
+        ],
+    })),
     useAuthData: jest.fn(() => ({ activeLoginid: null, logout: jest.fn() })),
+    useBalance: jest.fn(() => ({
+        data: {
+            balance: {
+                accounts: {
+                    CR90000383: {
+                        balance: 10302,
+                        converted_amount: 10302,
+                        currency: 'USD',
+                        demo_account: 0,
+                        status: 1,
+                        type: 'deriv',
+                    },
+                    VRTC90000221: {
+                        balance: 10000,
+                        converted_amount: 10000,
+                        currency: 'USD',
+                        demo_account: 1,
+                        status: 1,
+                        type: 'deriv',
+                    },
+                },
+                balance: 10302,
+                currency: 'USD',
+                loginid: 'CR90000383',
+                total: {
+                    deriv: {
+                        amount: 10302,
+                        currency: 'USD',
+                    },
+                    deriv_demo: {
+                        amount: 10000,
+                        currency: 'USD',
+                    },
+                    mt5: {
+                        amount: 0,
+                        currency: 'USD',
+                    },
+                    mt5_demo: {
+                        amount: 0,
+                        currency: 'USD',
+                    },
+                },
+            },
+        },
+    })),
 }));
 
 jest.mock('@deriv-com/ui', () => ({
@@ -18,22 +73,29 @@ jest.mock('@deriv-com/ui', () => ({
     useDevice: jest.fn(() => ({ isDesktop: true })),
 }));
 
+jest.mock('@deriv-com/translations', () => ({
+    useTranslations: jest.fn(() => ({ localize: jest.fn(text => text) })),
+}));
+
 describe('<AppHeader/>', () => {
     window.open = jest.fn();
 
     afterEach(() => {
         jest.clearAllMocks();
     });
-    it('should render the header', async () => {
+
+    it('should render the header and handle login when there are no P2P accounts', async () => {
         render(<AppHeader />);
         await userEvent.click(screen.getByRole('button', { name: 'Log in' }));
 
-        expect(window.open).toHaveBeenCalledWith(URLUtils.getOauthURL(), '_self');
+        expect(window.open).toHaveBeenCalledWith(expect.any(String), '_self');
     });
-    it('should handle the logout functionality if there is an active login id', async () => {
-        mockUseAuthData.mockReturnValue({ activeLoginid: '12345', logout: jest.fn() });
-        render(<AppHeader />);
 
+    it('should render the desktop header and manage account actions when logged in', async () => {
+        mockUseAuthData.mockReturnValue({ activeLoginid: '12345', logout: jest.fn() });
+
+        render(<AppHeader />);
+        screen.debug(undefined, Infinity);
         const logoutButton = screen.getByRole('button', { name: 'Logout' });
         const { logout } = mockUseAuthData();
         expect(logoutButton).toBeInTheDocument();
