d52ce24ca71e00d4ebbb3dabe53bda68661726cc
shayan khaleghparast <iman@regentmarkets.com>
2024-05-27 10:48:40 +0800

    fix: resolved pr comments

--- src/hooks/api/account/useActiveAccount.ts
+++ src/hooks/api/account/useActiveAccount.ts
@@ -1,12 +1,12 @@
 import { useMemo } from 'react';
+import { api } from '@/hooks';
 import { useAccountList, useAuthData } from '@deriv-com/api-hooks';
-import { useBalance } from '.';
 
 /** A custom hook that returns the account object for the current active account. */
 const useActiveAccount = () => {
     const { data, ...rest } = useAccountList();
     const { activeLoginid } = useAuthData();
-    const { data: balanceData } = useBalance();
+    const { data: balanceData } = api.account.useBalance();
     const active_account = useMemo(
         () => data?.find(account => account.loginid === activeLoginid),
         [activeLoginid, data]
