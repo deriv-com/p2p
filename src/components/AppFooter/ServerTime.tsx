0cef347dc397b8ad4b2fa6d90c3f3f8aa20acc23
Niloofar <niloofar.sadeghi@regentmarkets.com>
2024-05-28 18:43:26 +0800

    fix: review comments

--- src/components/AppFooter/ServerTime.tsx
+++ src/components/AppFooter/ServerTime.tsx
@@ -1,11 +1,12 @@
+import { DATE_TIME_FORMAT_WITH_GMT, DATE_TIME_FORMAT_WITH_OFFSET } from '@/constants';
 import { useSyncedTime } from '@/hooks';
 import { epochToLocal, epochToUTC } from '@/utils';
 import { Text, TooltipMenuIcon } from '@deriv-com/ui';
 
 export const ServerTime = () => {
     const time = useSyncedTime();
-    const UTCFormat = epochToUTC(time, 'YYYY-MM-DD HH:mm:ss [GMT]');
-    const localFormat = epochToLocal(time, 'YYYY-MM-DD HH:mm:ss Z');
+    const UTCFormat = epochToUTC(time, DATE_TIME_FORMAT_WITH_GMT);
+    const localFormat = epochToLocal(time, DATE_TIME_FORMAT_WITH_OFFSET);
 
     return (
         <TooltipMenuIcon as='div' className='app-footer__icon' disableHover tooltipContent={localFormat}>
--- src/hooks/custom-hooks/useSyncedTime.ts
+++ src/hooks/custom-hooks/useSyncedTime.ts
@@ -1,6 +1,27 @@
 import { useEffect, useState } from 'react';
 import { useTime } from '@deriv-com/api-hooks';
 
+/**
+ * Custom React hook that syncs with server time and keeps it updated.
+ *
+ * This hook fetches the current server time at regular intervals and maintains a local
+ * time state that is updated every second. The server time is refetched every 30 seconds.
+ *
+ * @returns {number} The current server time in seconds since the Unix epoch.
+ *
+ * @example
+ * // Example usage in a functional component
+ * import React from 'react';
+ * import useSyncedTime from './useSyncedTime';
+ *
+ * const ServerTimeDisplay = () => {
+ *     const serverTime = useSyncedTime();
+ *
+ *     return <div>Current Server Time: {new Date(serverTime * 1000).toLocaleString()}</div>;
+ * };
+ *
+ * export default ServerTimeDisplay;
+ */
 const useSyncedTime = () => {
     const currentDate = Date.now() / 1000;
     const [serverTime, setServerTime] = useState(currentDate);
@@ -18,8 +39,7 @@ const useSyncedTime = () => {
         }
 
         return () => clearInterval(timeInterval);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [data]);
+    }, [data, currentDate]);
 
     return serverTime;
 };
