1e1f8cad1b57a1c9e7643fb8c2dbd97770a60f5e
Niloofar <niloofar.sadeghi@regentmarkets.com>
2024-05-30 15:48:20 +0800

    fix: review comments

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,3 +1,5 @@
+import { URLConstants } from '@deriv-com/utils';
+
 export const BUY_SELL_URL = '/buy-sell';
 export const ORDERS_URL = '/orders';
 export const MY_ADS_URL = '/my-ads';
@@ -6,8 +8,8 @@ export const ADVERTISER_URL = '/advertiser';
 export const ENDPOINT = '/endpoint';
 
 // TODO move these to deriv-utils library
-export const ACCOUNT_LIMITS = 'https://app.deriv.com/account/account-limits';
-export const DERIV_COM = 'https://deriv.com/';
-export const HELP_CENTRE = 'https://deriv.com/help-centre/';
-export const RESPONSIBLE = 'https://deriv.com/responsible/';
+export const ACCOUNT_LIMITS = `${URLConstants.derivAppProduction}/account/account-limits`;
+export const DERIV_COM = URLConstants.derivComProduction;
+export const HELP_CENTRE = `${URLConstants.derivComProduction}/help-centre/`;
+export const RESPONSIBLE = `${URLConstants.derivComProduction}/responsible/`;
 export const WHATSAPP = 'https://api.whatsapp.com/send/?phone=35699578341&text&type=phone_number&app_absent=0';
