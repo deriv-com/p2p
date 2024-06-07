55640a332648d73327772ab73dfd5bbb31fbd2fe
shayan khaleghparast <iman@regentmarkets.com>
2024-06-07 12:30:52 +0800

    feat: implement redirection to oauth login page [WIP]

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+/* eslint-disable no-console */
+import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryParamProvider } from 'use-query-params';
 import { ReactRouter5Adapter } from 'use-query-params/adapters/react-router-5';
 import { AppFooter, AppHeader, DerivIframe } from '@/components';
+import { useAccountList, useAuthData } from '@deriv-com/api-hooks';
 import { initializeI18n, TranslationProvider } from '@deriv-com/translations';
 import { useDevice } from '@deriv-com/ui';
 import AppContent from './routes/AppContent';
+import { getOauthUrl } from './constants';
 
 const { VITE_CROWDIN_BRANCH_NAME, VITE_PROJECT_NAME, VITE_TRANSLATIONS_CDN_URL } = import.meta.env;
 const i18nInstance = initializeI18n({
@@ -13,6 +17,23 @@ const i18nInstance = initializeI18n({
 
 const App = () => {
     const { isDesktop } = useDevice();
+    const { data: accountList } = useAccountList();
+    const { activeLoginid, isAuthorized } = useAuthData();
+
+    const oauthUrl = getOauthUrl();
+    useEffect(() => {
+        const hasActiveLoginid = !!activeLoginid;
+        const hasAccounts = !!accountList?.length;
+        const shouldRedirectToLogin = !isAuthorized && !hasActiveLoginid && !hasAccounts;
+        console.log('isAuthorized', isAuthorized);
+        console.log('hasActiveLoginid', hasActiveLoginid);
+        console.log('hasAccounts', hasAccounts);
+        console.log('shouldRedirectToLogin', shouldRedirectToLogin);
+        console.log('====');
+        // if (shouldRedirectToLogin) {
+        //     window.open(oauthUrl, '_self');
+        // }
+    }, [accountList, activeLoginid, isAuthorized, oauthUrl]);
 
     return (
         <BrowserRouter>
