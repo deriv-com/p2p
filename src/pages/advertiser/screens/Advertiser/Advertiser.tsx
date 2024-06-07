9fa3ef2b7ebc15821117660a527f44c9b0b80b0a
Nada <nada.ajinas@deriv.com>
2024-06-07 15:49:48 +0400

    fix: ui issues in buy sell flow

--- src/pages/advertiser/screens/Advertiser/Advertiser.tsx
+++ src/pages/advertiser/screens/Advertiser/Advertiser.tsx
@@ -48,6 +48,7 @@ const Advertiser = () => {
                         />
                     ),
                 })}
+                size={isMobile ? 'lg' : 'md'}
                 weight='bold'
             />
             <AdvertiserBlockOverlay
