36dc2b1a8073adb322ed305663e25b60ebd7ae6a
shayan khaleghparast <iman@regentmarkets.com>
2024-06-12 11:27:14 +0800

    fix: fixing iframe load issue

--- src/components/DerivIframe/DerivIframe.tsx
+++ src/components/DerivIframe/DerivIframe.tsx
@@ -1,10 +1,15 @@
-const DerivIframe = () => (
-    <iframe
-        id='localstorage-sync'
-        sandbox='allow-same-origin allow-scripts'
-        src='../../../../localstorage-sync.html'
-        style={{ display: 'none', visibility: 'hidden' }}
-    />
-);
+import { URLConstants } from '@deriv-com/utils';
+
+const DerivIframe = () => {
+    const origin = URLConstants.derivP2pProduction;
+    return (
+        <iframe
+            id='localstorage-sync'
+            sandbox='allow-same-origin allow-scripts'
+            src={`${origin}/public/localstorage-sync.html`}
+            style={{ display: 'none', visibility: 'hidden' }}
+        />
+    );
+};
 
 export default DerivIframe;
