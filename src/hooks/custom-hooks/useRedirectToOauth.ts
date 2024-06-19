5b0168490e4a1a8b3fbcf1f6543f5fda5538e5b5
Shayan Khaleghparast <100833613+shayan-deriv@users.noreply.github.com>
2024-06-19 18:24:51 +0800

    Shayan/feq 2275/add skeleton loader to header (#137)
    * feat: added preloader component [WIP]
    
    * feat: added preloader component [WIP]
    
    * feat: added skeleton loader for header
    
    * chore: removed nested ternary
    
    * fix: addressed pr comments
    
    * fix: addressed pr comments
    
    * fix: addressed pr comments
    
    * fix: fixed test issue
    
    * chore: removed console.log

--- src/hooks/custom-hooks/useRedirectToOauth.ts
+++ src/hooks/custom-hooks/useRedirectToOauth.ts
@@ -5,19 +5,21 @@ import { useAuthData } from '@deriv-com/api-hooks';
 
 const useRedirectToOauth = () => {
     const [shouldRedirect, setShouldRedirect] = useState(false);
-    const { isAuthorized, isAuthorizing } = useAuthData();
+    const { error, isAuthorized, isAuthorizing } = useAuthData();
     const isEndpointPage = getCurrentRoute() === 'endpoint';
-
     const oauthUrl = getOauthUrl();
     const redirectToOauth = useCallback(() => {
         shouldRedirect && window.open(oauthUrl, '_self');
     }, [oauthUrl, shouldRedirect]);
 
     useEffect(() => {
-        if (!isEndpointPage && !isAuthorized && !isAuthorizing) {
+        if (
+            (!isEndpointPage && !isAuthorized && !isAuthorizing) ||
+            (!isEndpointPage && error?.code === 'InvalidToken')
+        ) {
             setShouldRedirect(true);
         }
-    }, [isAuthorized, isAuthorizing, isEndpointPage, oauthUrl]);
+    }, [error, isAuthorized, isAuthorizing, isEndpointPage, oauthUrl]);
 
     return {
         redirectToOauth,
