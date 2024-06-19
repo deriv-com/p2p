658e1a41feccf939d5d460a48ea0043a34ae00ef
shayan khaleghparast <iman@regentmarkets.com>
2024-06-19 16:06:13 +0800

    fix: addressed pr comments

--- src/components/AppHeader/AccountSwitcher/index.tsx
+++ src/components/AppHeader/AccountSwitcher/index.tsx
@@ -4,11 +4,11 @@ import { AccountSwitcher as UIAccountSwitcher } from '@deriv-com/ui';
 import { FormatUtils } from '@deriv-com/utils';
 
 type TActiveAccount = NonNullable<ReturnType<typeof useActiveAccount>['data']>;
-type AccountSwitcherProps = {
+type TAccountSwitcherProps = {
     account: TActiveAccount;
 };
 
-export const AccountSwitcher = ({ account }: AccountSwitcherProps) => {
+export const AccountSwitcher = ({ account }: TAccountSwitcherProps) => {
     const activeAccount = {
         balance: FormatUtils.formatMoney(account?.balance ?? 0),
         currency: account?.currency || 'USD',
