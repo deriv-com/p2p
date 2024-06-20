ef0e7076000e38336a9acfd487dd538374114116
ameerul-deriv <ameerul@deriv.com>
2024-06-20 13:14:34 +0800

    chore: used FormatMoney from utils instead of numberToCurrencyText, deleted currency.ts

--- src/utils/__tests__/currency.spec.ts
+++
@@ -1,9 +0,0 @@
-import { numberToCurrencyText } from '../currency';
-
-describe('currency', () => {
-    describe('numberToCurrencyText', () => {
-        it('should convert a number to US-supported currency format', () => {
-            expect(numberToCurrencyText(10000)).toBe('10,000.00');
-        });
-    });
-});
