216498552416467bf246095e5c138abb9abed682
shayan khaleghparast <iman@regentmarkets.com>
2024-06-21 10:09:57 +0800

    chore: fixed Endpoint page test issue

--- src/pages/endpoint/screens/Endpoint/__tests__/Endpoint.spec.tsx
+++ src/pages/endpoint/screens/Endpoint/__tests__/Endpoint.spec.tsx
@@ -3,14 +3,14 @@ import userEvent from '@testing-library/user-event';
 import Endpoint from '../Endpoint';
 
 describe('<Endpoint />', () => {
-    // it('should render the endpoint component', () => {
-    //     render(<Endpoint />);
+    it('should render the endpoint component', () => {
+        render(<Endpoint />);
 
-    //     expect(screen.getByText('Change API endpoint')).toBeInTheDocument();
-    //     expect(screen.getByText('Server')).toBeInTheDocument();
-    //     expect(screen.getByText('OAuth App ID')).toBeInTheDocument();
-    //     expect(screen.getByRole('button', { name: 'Submit' })).toBeInTheDocument();
-    // });
+        expect(screen.getByText('Change API endpoint')).toBeInTheDocument();
+        expect(screen.getByText('Server')).toBeInTheDocument();
+        expect(screen.getByText('OAuth App ID')).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: 'Submit' })).toBeInTheDocument();
+    });
 
     it('should handle form submission', async () => {
         render(<Endpoint />);
@@ -23,7 +23,7 @@ describe('<Endpoint />', () => {
         await userEvent.type(appIdInput, '123');
         await userEvent.click(submitButton);
 
-        expect(localStorage.getItem('config.server_url')).toBe('qa10.deriv.dev');
-        expect(localStorage.getItem('config.app_id')).toBe('123');
+        expect(JSON.parse(localStorage.getItem('config.server_url') || '')).toBe('qa10.deriv.dev');
+        expect(JSON.parse(localStorage.getItem('config.app_id') || '')).toBe('123');
     });
 });
