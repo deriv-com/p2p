1a84d75642b5bb72562bf42f5838065a9e243eea
Nada <nada.ajinas@deriv.com>
2024-06-26 10:32:09 +0400

    chore: add unit tests for advertiser relations

--- src/hooks/api/counterparty/p2p-advertiser-relations/useAdvertiserRelations.ts
+++ src/hooks/api/counterparty/p2p-advertiser-relations/useAdvertiserRelations.ts
@@ -4,27 +4,27 @@ import useInvalidateQuery from '../../useInvalidateQuery';
 /** This hook returns favourite and blocked advertisers and the mutation function to update the block list of the current user. */
 const useAdvertiserRelations = () => {
     const invalidate = useInvalidateQuery();
-    const { data, mutate, ...mutate_rest } = useP2PAdvertiserRelations({
+    const { data, mutate, ...mutateRest } = useP2PAdvertiserRelations({
         onSuccess: () => {
             invalidate('p2p_advertiser_relations');
             invalidate('p2p_advertiser_list');
         },
     });
 
-    const advertiser_relations = data;
+    const advertiserRelations = data;
 
     return {
         /** Blocked advertisers by the current user. */
-        blocked_advertisers: advertiser_relations?.blocked_advertisers,
+        blockedAdvertisers: advertiserRelations?.blocked_advertisers,
         /** P2P advertiser relations information. */
-        data: advertiser_relations,
+        data: advertiserRelations,
         /** Favourite advertisers of the current user. */
-        favourite_advertisers: advertiser_relations?.favourite_advertisers,
+        favouriteAdvertisers: advertiserRelations?.favourite_advertisers,
 
         /** The mutation function to update (add/remove) the currrent user's block list.  */
         mutate,
         /** The mutation related information. */
-        mutation: mutate_rest,
+        mutation: mutateRest,
     };
 };
 
