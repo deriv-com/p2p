0d8203730ffd594dab03b347022dbf11b356afaf
Nada <nada.ajinas@deriv.com>
2024-07-05 12:24:44 +0400

    fix: update test file

--- src/components/BlockedScenarios/__tests__/BlockedScenarios.spec.tsx
+++ src/components/BlockedScenarios/__tests__/BlockedScenarios.spec.tsx
@@ -1,4 +1,6 @@
+import { URLConstants } from '@deriv-com/utils';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import BlockedScenarios from '../BlockedScenarios';
 
 jest.mock('@deriv-com/ui', () => ({
@@ -6,19 +8,36 @@ jest.mock('@deriv-com/ui', () => ({
     useDevice: () => ({ isDesktop: true }),
 }));
 
+const mockFn = jest.fn();
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useHistory: () => ({
+        push: mockFn,
+    }),
+}));
+
 describe('BlockedScenarios', () => {
-    it('should render the correct message for demo account', () => {
+    it('should render the correct message for demo account', async () => {
         render(<BlockedScenarios type='demo' />);
         expect(screen.getByText('You are using a demo account')).toBeVisible();
+        const button = screen.getByRole('button', { name: 'Switch to real USD account' });
+        await userEvent.click(button);
+        expect(mockFn).toHaveBeenCalledWith(URLConstants.derivAppProduction);
     });
 
-    it('should render the correct message for non-USD account', () => {
+    it('should render the correct message for non-USD account', async () => {
         render(<BlockedScenarios type='nonUSD' />);
         expect(screen.getByText('You have no Real USD account')).toBeVisible();
+        const button = screen.getByRole('button', { name: 'Create real USD account' });
+        await userEvent.click(button);
+        expect(mockFn).toHaveBeenCalledWith(URLConstants.derivAppProduction);
     });
 
-    it('should render the correct message for crypto account', () => {
+    it('should render the correct message for crypto account', async () => {
         render(<BlockedScenarios type='crypto' />);
         expect(screen.getByText('Crypto is not supported for Deriv P2P!')).toBeVisible();
+        const button = screen.getByRole('button', { name: 'Switch to real USD account' });
+        await userEvent.click(button);
+        expect(mockFn).toHaveBeenCalledWith(URLConstants.derivAppProduction);
     });
 });
--- src/utils/__tests__/account.spec.ts
+++ src/utils/__tests__/account.spec.ts
@@ -3,14 +3,17 @@ import { getBlockedType } from '../account';
 describe('getBlockedType', () => {
     it('should return the correct blocked type', () => {
         const mockData = {
-            currency: 'BTC',
-            is_virtual: 1,
+            currency: 'USD',
+            is_virtual: 0,
         };
 
         // @ts-expect-error only required fields are provided
-        expect(getBlockedType({ ...mockData })).toEqual('crypto');
+        expect(getBlockedType({ ...mockData, currency_type: 'crypto' })).toEqual('crypto');
 
         // @ts-expect-error only required fields are provided
         expect(getBlockedType({ ...mockData, currency: 'USD', is_virtual: 1 })).toEqual('demo');
+
+        // @ts-expect-error only required fields are provided
+        expect(getBlockedType({ ...mockData, currency: 'EUR' })).toEqual('nonUSD');
     });
 });
