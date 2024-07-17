864f0b0c61a336c864ae1171051d2486c30cb93e
shayan khaleghparast <iman@regentmarkets.com>
2024-07-17 13:29:02 +0800

    chore: resolved pr comments

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryParamProvider } from 'use-query-params';
 import { ReactRouter5Adapter } from 'use-query-params/adapters/react-router-5';
-import { AppFooter, AppHeader, DerivIframe } from '@/components';
+import { AppFooter, AppHeader, DerivIframe, ErrorBoundary } from '@/components';
 import { useRedirectToOauth, useTrackjs } from '@/hooks';
 import AppContent from '@/routes/AppContent';
 import { initializeI18n, TranslationProvider } from '@deriv-com/translations';
 import { Loader, useDevice } from '@deriv-com/ui';
-import { ErrorBoundary } from '@/components';
 
 const { VITE_CROWDIN_BRANCH_NAME, VITE_PROJECT_NAME, VITE_TRANSLATIONS_CDN_URL } = process.env;
 const i18nInstance = initializeI18n({
