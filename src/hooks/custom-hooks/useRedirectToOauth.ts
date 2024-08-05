b6b222b4918eb1c511ccccdccb8e5e45af5560ec
Thisyahlen <thisyahlen@regentmarkets.com>
2024-08-05 11:50:59 +0800

    chore: logout iframe oauth2

--- src/hooks/custom-hooks/useRedirectToOauth.ts
+++ src/hooks/custom-hooks/useRedirectToOauth.ts
@@ -8,8 +8,23 @@ const useRedirectToOauth = () => {
     const { error, isAuthorized, isAuthorizing } = useAuthData();
     const isEndpointPage = getCurrentRoute() === 'endpoint';
     const oauthUrl = getOauthUrl();
+
     const redirectToOauth = useCallback(() => {
-        shouldRedirect && window.open(oauthUrl, '_self');
+        if (shouldRedirect) {
+            let iframe = document.getElementById('logout-iframe') as HTMLIFrameElement;
+            if (!iframe) {
+                iframe = document.createElement('iframe');
+                iframe.id = 'logout-iframe';
+                iframe.style.display = 'none';
+                document.body.appendChild(iframe);
+            }
+
+            iframe.src = 'https://qa101.deriv.dev/oauth2/sessions/logout';
+
+            iframe.onload = () => {
+                window.location.href = oauthUrl;
+            };
+        }
     }, [oauthUrl, shouldRedirect]);
 
     useEffect(() => {
@@ -20,6 +35,10 @@ const useRedirectToOauth = () => {
             setShouldRedirect(true);
         }
     }, [error, isAuthorized, isAuthorizing, isEndpointPage, oauthUrl]);
+
+    useEffect(() => {
+        redirectToOauth();
+    }, [redirectToOauth]);
 
     return {
         redirectToOauth,
