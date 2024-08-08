401a6ceea73d72c79c84aac060abb82271da8da1
amam-deriv <mustofa@deriv.com>
2024-08-08 11:37:29 +0800

    feat(amam): complete logout scenario

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,4 +1,5 @@
-import { getOauthUrl } from '@/constants';
+import { useEffect } from 'react';
+import { getOAuthLogoutUrl, getOAuthOrigin, getOauthUrl } from '@/constants';
 import { api, useRedirectToOauth } from '@/hooks';
 import { getCurrentRoute } from '@/utils';
 import { StandaloneCircleUserRegularIcon } from '@deriv/quill-icons';
@@ -15,6 +16,11 @@ import { Notifications } from './Notifications';
 import { PlatformSwitcher } from './PlatformSwitcher';
 import './AppHeader.scss';
 
+type MessageEvent = {
+    data: 'logout_complete' | 'logout_error';
+    origin: string;
+};
+
 // TODO: handle local storage values not updating after changing local storage values
 const AppHeader = () => {
     const { isDesktop } = useDevice();
@@ -24,6 +30,52 @@ const AppHeader = () => {
     const { data: activeAccount } = api.account.useActiveAccount();
     const { localize } = useTranslations();
     const oauthUrl = getOauthUrl();
+
+    const WSLogoutAndRedirect = async () => {
+        await logout();
+        redirectToOauth();
+    };
+
+    useEffect(() => {
+        const onMessage = async (event: MessageEvent) => {
+            const allowedOrigin = getOAuthOrigin();
+            if (allowedOrigin === event.origin) {
+                if (event.data === 'logout_complete') {
+                    WSLogoutAndRedirect();
+                } else {
+                    // eslint-disable-next-line no-console
+                    console.warn('Unexpected message received: ', event.data);
+                }
+            } else {
+                // eslint-disable-next-line no-console
+                console.warn('Unexpected postmessage origin: ', event.origin);
+            }
+        };
+
+        window.addEventListener('message', onMessage);
+
+        return () => {
+            window.removeEventListener('message', onMessage);
+        };
+    }, []);
+
+    const handleLogout = async () => {
+        let iframe: HTMLIFrameElement | null = document.getElementById('logout-iframe') as HTMLIFrameElement;
+        if (!iframe) {
+            iframe = document.createElement('iframe');
+            iframe.id = 'logout-iframe';
+            iframe.style.display = 'none';
+            document.body.appendChild(iframe);
+        }
+
+        iframe.src = getOAuthLogoutUrl();
+
+        // Reject the promise if there is an error
+        iframe.onerror = error => {
+            // eslint-disable-next-line no-console
+            console.error('There has been a problem with the logout: ', error);
+        };
+    };
 
     const renderAccountSection = () => {
         if (!isEndpointPage && !activeAccount) {
@@ -47,14 +99,7 @@ const AppHeader = () => {
                         </TooltipMenuIcon>
                     )}
                     <AccountSwitcher account={activeAccount!} />
-                    <Button
-                        className='mr-6'
-                        onClick={async () => {
-                            await logout();
-                            redirectToOauth();
-                        }}
-                        size='md'
-                    >
+                    <Button className='mr-6' onClick={handleLogout} size='md'>
                         <Text size='sm' weight='bold'>
                             {localize('Logout')}
                         </Text>
@@ -65,7 +110,7 @@ const AppHeader = () => {
 
         return (
             <Button
-                className='min-w-36'
+                className='w-36'
                 color='primary-light'
                 onClick={() => window.open(oauthUrl, '_self')}
                 variant='ghost'
