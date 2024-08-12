21ff24d190d1b6f67d342a3f67553dbaa9196c6f
amam-deriv <mustofa@deriv.com>
2024-08-12 13:25:27 +0800

    fix(amam): add lang in the oauth url

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -31,7 +31,7 @@ const SocketURL = {
     [URLConstants.derivP2pStaging]: 'red.derivws.com',
 };
 
-export const getServerUrlAndAppId = () => {
+export const getServerInfo = () => {
     const origin = window.location.origin;
     const hostname = window.location.hostname;
 
@@ -55,26 +55,28 @@ export const getServerUrlAndAppId = () => {
     const serverUrl = /qa/.test(String(storedServerUrl)) ? storedServerUrl : 'oauth.deriv.com';
 
     const appId = LocalStorageUtils.getValue<string>(LocalStorageConstants.configAppId);
+    const lang = LocalStorageUtils.getValue<string>(LocalStorageConstants.i18nLanguage);
 
     return {
         appId,
+        lang,
         serverUrl,
     };
 };
 
 export const getOauthUrl = () => {
-    const { appId, serverUrl } = getServerUrlAndAppId();
+    const { appId, lang, serverUrl } = getServerInfo();
 
     const oauthUrl =
         appId && serverUrl
-            ? `https://${serverUrl}/oauth2/authorize?app_id=${appId}&l=EN&&brand=deriv`
+            ? `https://${serverUrl}/oauth2/authorize?app_id=${appId}&l=${lang ?? 'EN'}&&brand=deriv`
             : URLUtils.getOauthURL();
 
     return oauthUrl;
 };
 
 export const getOAuthLogoutUrl = () => {
-    const { appId, serverUrl } = getServerUrlAndAppId();
+    const { appId, serverUrl } = getServerInfo();
 
     const oauthUrl = appId && serverUrl ? `https://${serverUrl}/oauth2/sessions/logout` : DEFAULT_OAUTH_LOGOUT_URL;
 
@@ -82,7 +84,7 @@ export const getOAuthLogoutUrl = () => {
 };
 
 export const getOAuthOrigin = () => {
-    const { appId, serverUrl } = getServerUrlAndAppId();
+    const { appId, serverUrl } = getServerInfo();
 
     const oauthUrl = appId && serverUrl ? `https://${serverUrl}` : DEFAULT_OAUTH_ORIGIN_URL;
 
