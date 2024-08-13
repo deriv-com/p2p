79a682aabb5cdfd7dbf82f99f6d68a5ba6658742
ameerul-deriv <ameerul@deriv.com>
2024-08-13 15:59:28 +0800

    fix: failing tests for buysellform

--- src/components/BuySellForm/__tests__/BuySellForm.spec.tsx
+++ src/components/BuySellForm/__tests__/BuySellForm.spec.tsx
@@ -94,6 +94,10 @@ const mockAdvertInfo = {
     unsubscribe: jest.fn(),
 };
 
+const mockUseBalance = {
+    data: { balance: 100 },
+};
+
 const mockInvalidate = jest.fn();
 
 jest.mock('@/hooks/api/useInvalidateQuery', () => () => mockInvalidate);
@@ -101,6 +105,9 @@ jest.mock('@/hooks/api/useInvalidateQuery', () => () => mockInvalidate);
 jest.mock('@/hooks', () => ({
     ...jest.requireActual('@/hooks'),
     api: {
+        account: {
+            useBalance: jest.fn(() => mockUseBalance),
+        },
         advert: {
             useSubscribe: jest.fn(() => mockAdvertInfo),
         },
