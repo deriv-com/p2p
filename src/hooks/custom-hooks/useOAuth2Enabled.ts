77aac04c237c74736aeeed383bf5c6485e569cec
amam-deriv <mustofa@deriv.com>
2024-08-16 16:36:18 +0800

    fix: use number instead of string

--- src/hooks/custom-hooks/useOAuth2Enabled.ts
+++ src/hooks/custom-hooks/useOAuth2Enabled.ts
@@ -5,7 +5,7 @@ import useGrowthbookGetFeatureValue from './useGrowthbookGetFeatureValue';
 type hydraBEValue = {
     // list of app ids having oauth2 enabled
     disabled_for: string[];
-    enabled_for: string[]; // list of blocked app ids
+    enabled_for: number[]; // list of blocked app ids
     official_apps_enabled: boolean;
 };
 
@@ -28,10 +28,12 @@ const useOAuth2Enabled = () => {
 
     useEffect(() => {
         if (OAuth2EnabledAppsInitialised) {
-            const FEHydraAppIds = OAuth2EnabledApps[1]?.enabled_for || [];
-            setIsOauth2Enabled(FEHydraAppIds.includes(appId as string));
+            const FEHydraAppIds = OAuth2EnabledApps.length
+                ? OAuth2EnabledApps[OAuth2EnabledApps.length - 1]?.enabled_for ?? []
+                : [];
+            setIsOauth2Enabled(FEHydraAppIds.includes(+(appId as string)));
         }
-    }, [OAuth2EnabledAppsInitialised]);
+    }, [OAuth2EnabledAppsInitialised, OAuth2EnabledApps]);
 
     return [isOauth2Enabled];
 };
