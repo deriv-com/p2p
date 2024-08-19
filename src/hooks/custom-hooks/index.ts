2d27da6df5f64d48f19ee876b6d9583733a555e9
Nada <nada.ajinas@deriv.com>
2024-08-19 12:46:50 +0400

    fix: icon direction change

--- src/hooks/custom-hooks/index.ts
+++ src/hooks/custom-hooks/index.ts
@@ -10,6 +10,7 @@ export { default as useHandleRouteChange } from './useHandleRouteChange';
 export { default as useIsAdvertiser } from './useIsAdvertiser';
 export { default as useIsAdvertiserBarred } from './useIsAdvertiserBarred';
 export { default as useIsP2PBlocked } from './useIsP2PBlocked';
+export { default as useIsRtl } from './useIsRtl';
 export { default as useLiveChat } from './useLiveChat';
 export { default as useModalManager } from './useModalManager';
 export { default as useNavigatorOnline } from './useNavigatorOnline';
