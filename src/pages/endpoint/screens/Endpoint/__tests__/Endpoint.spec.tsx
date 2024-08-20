ea1985badc60e6900791a533420240470c60d698
ameerul-deriv <ameerul@deriv.com>
2024-08-20 15:03:43 +0800

    fix: endpoint test cases

--- src/pages/endpoint/screens/Endpoint/__tests__/Endpoint.spec.tsx
+++ src/pages/endpoint/screens/Endpoint/__tests__/Endpoint.spec.tsx
@@ -10,6 +10,7 @@ describe('<Endpoint />', () => {
         expect(screen.getByText('Server')).toBeInTheDocument();
         expect(screen.getByText('OAuth App ID')).toBeInTheDocument();
         expect(screen.getByRole('button', { name: 'Submit' })).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: 'Reset to original settings' })).toBeInTheDocument();
     });
 
     it('should handle form submission', async () => {
@@ -25,5 +26,20 @@ describe('<Endpoint />', () => {
 
         expect(JSON.parse(localStorage.getItem('config.server_url') || '')).toBe('qa10.deriv.dev');
         expect(JSON.parse(localStorage.getItem('config.app_id') || '')).toBe('123');
+    });
+
+    it('should reset the server_url and app_id when user clicks on the reset button', async () => {
+        render(<Endpoint />);
+
+        const serverUrlInput = screen.getByTestId('dt_endpoint_server_url_input');
+        const appIdInput = screen.getByTestId('dt_endpoint_app_id_input');
+        const resetButton = screen.getByRole('button', { name: 'Reset to default' });
+
+        await userEvent.type(serverUrlInput, 'qa10.deriv.dev');
+        await userEvent.type(appIdInput, '123');
+        await userEvent.click(resetButton);
+
+        expect(JSON.parse(localStorage.getItem('config.server_url') || '')).toBe('');
+        expect(JSON.parse(localStorage.getItem('config.app_id') || '')).toBe('');
     });
 });
