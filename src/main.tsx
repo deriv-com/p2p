43078b8dadce014a0ef30f5ff90240a92e939d3c
Thisyahlen <thisyahlen@regentmarkets.com>
2024-09-12 10:26:52 +0800

    chore: use auth-library

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AppDataProvider } from '@deriv-com/api-hooks';
+import { OAuth2Provider } from '@deriv-com/auth-client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
+import { getOauthUrl } from './constants';
 import './main.scss';
+
+const oauthUrl = getOauthUrl();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <QueryClientProvider client={new QueryClient()}>
             <AppDataProvider>
-                <App />
+                <OAuth2Provider oauthUrl={oauthUrl}>
+                    <App />
+                </OAuth2Provider>
             </AppDataProvider>
         </QueryClientProvider>
     </React.StrictMode>
