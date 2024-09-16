46a70716a15ba36f67a0ddfb6467d15f61ba17b5
Nada <nada.ajinas@deriv.com>
2024-09-16 16:11:07 +0400

    fix: floating badge position fix

--- src/components/BuySellForm/BuySellData/BuySellData.tsx
+++ src/components/BuySellForm/BuySellData/BuySellData.tsx
@@ -76,7 +76,8 @@ const BuySellData = forwardRef<HTMLDivElement, TBuySellDataProps>(
                                 </Text>
                                 {isFloating && (
                                     <Tooltip
-                                        tooltipContainerClassName='w-72 mb-[-0.8rem] text-center'
+                                        hideTooltip={!isDesktop}
+                                        tooltipContainerClassName='w-72 text-center'
                                         tooltipContent={
                                             <Text size={labelSize}>
                                                 <Localize i18n_default_text='Floating exchange rate shifts with market fluctuations.' />
