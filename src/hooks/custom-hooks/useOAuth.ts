a7fff1e7d57897a71c2f9a15edc66d5f03a394fd
Thisyahlen <thisyahlen@regentmarkets.com>
2024-09-20 11:16:03 +0800

    test: add logout oauth2

--- src/hooks/custom-hooks/useOAuth.ts
+++ src/hooks/custom-hooks/useOAuth.ts
@@ -1,18 +1,18 @@
-import { useCallback, useEffect } from 'react';
-import { getOAuthLogoutUrl, getOAuthOrigin, getOauthUrl } from '@/constants';
+import { useCallback } from 'react';
+import { getOauthUrl } from '@/constants';
 import { getCurrentRoute } from '@/utils';
 import { useAuthData } from '@deriv-com/api-hooks';
-import useOAuth2Enabled from './useOAuth2Enabled';
-
-type MessageEvent = {
-    data: 'logout_complete' | 'logout_error';
-    origin: string;
-};
+import { useOAuth2 } from '@deriv-com/auth-client';
+import useGrowthbookGetFeatureValue from './useGrowthbookGetFeatureValue';
 
 type UseOAuthReturn = {
     oAuthLogout: () => void;
     onRenderAuthCheck: () => void;
 };
+
+type hydraBEApps = {
+    enabled_for: number[];
+}[];
 
 /**
  * useOAuth - hooks to help with OAuth function such as logout and check auth state during render
@@ -20,69 +20,25 @@ type UseOAuthReturn = {
  * @returns {UseOAuthReturn}
  */
 const useOAuth = (): UseOAuthReturn => {
-    const [isOAuth2Enabled] = useOAuth2Enabled();
+    const [OAuth2EnabledApps, OAuth2EnabledAppsInitialised] = useGrowthbookGetFeatureValue<hydraBEApps>({
+        featureFlag: 'hydra_be',
+    });
+
+    const config = {
+        OAuth2EnabledApps,
+        OAuth2EnabledAppsInitialised,
+    };
+
     const { logout } = useAuthData();
-    const { error, isAuthorized, isAuthorizing } = useAuthData();
-    const isEndpointPage = getCurrentRoute() === 'endpoint';
-    const oauthUrl = getOauthUrl();
 
     const WSLogoutAndRedirect = async () => {
         await logout();
         window.open(oauthUrl, '_self');
     };
-
-    useEffect(() => {
-        const onMessage = async (event: MessageEvent) => {
-            const allowedOrigin = getOAuthOrigin();
-            if (allowedOrigin === event.origin) {
-                if (event.data === 'logout_complete') {
-                    WSLogoutAndRedirect();
-                } else {
-                    // eslint-disable-next-line no-console
-                    console.warn('Unexpected message received: ', event.data);
-                }
-            } else {
-                // eslint-disable-next-line no-console
-                console.warn('Unexpected postmessage origin: ', event.origin);
-            }
-        };
-
-        window.addEventListener('message', onMessage);
-
-        return () => {
-            window.removeEventListener('message', onMessage);
-        };
-    }, []);
-
-    const OAuth2Logout = async () => {
-        let iframe: HTMLIFrameElement | null = document.getElementById('logout-iframe') as HTMLIFrameElement;
-        if (!iframe) {
-            iframe = document.createElement('iframe');
-            iframe.id = 'logout-iframe';
-            iframe.style.display = 'none';
-            document.body.appendChild(iframe);
-
-            // backend response message timeout, setting this as 10 seconds
-            setTimeout(() => {
-                WSLogoutAndRedirect();
-            }, 10000);
-        }
-
-        iframe.src = getOAuthLogoutUrl();
-
-        iframe.onerror = error => {
-            // eslint-disable-next-line no-console
-            console.error('There has been a problem with the logout: ', error);
-        };
-    };
-
-    const oAuthLogout = useCallback(async () => {
-        if (isOAuth2Enabled) {
-            OAuth2Logout();
-        } else {
-            WSLogoutAndRedirect();
-        }
-    }, [isOAuth2Enabled]);
+    const { OAuth2Logout: oAuthLogout } = useOAuth2(config, WSLogoutAndRedirect);
+    const { error, isAuthorized, isAuthorizing } = useAuthData();
+    const isEndpointPage = getCurrentRoute() === 'endpoint';
+    const oauthUrl = getOauthUrl();
 
     const onRenderAuthCheck = useCallback(() => {
         if (
