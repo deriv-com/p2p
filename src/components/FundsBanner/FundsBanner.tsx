bfc0ea4b2f5ebb8aa4a93b1689e6912d14f81abf
ameerul-deriv <ameerul@deriv.com>
2024-11-11 11:55:03 +0800

    chore: added test cases for FundsBanner and FundsModal

--- src/components/FundsBanner/FundsBanner.tsx
+++ src/components/FundsBanner/FundsBanner.tsx
@@ -2,12 +2,13 @@ import { FundsModal } from '@/components/Modals';
 import { useModalManager } from '@/hooks/custom-hooks';
 import { LabelPairedCircleInfoLgBoldIcon } from '@deriv/quill-icons';
 import { Localize } from '@deriv-com/translations';
-import { InlineMessage, Text, useDevice } from '@deriv-com/ui';
+import { Button, InlineMessage, Text, useDevice } from '@deriv-com/ui';
 import './FundsBanner.scss';
 
 const FundsBanner = () => {
     const { isDesktop } = useDevice();
     const { hideModal, isModalOpenFor, showModal } = useModalManager();
+    const textSize = isDesktop ? 'sm' : 'md';
 
     return (
         <div className='funds-banner'>
@@ -16,18 +17,21 @@ const FundsBanner = () => {
                 icon={<LabelPairedCircleInfoLgBoldIcon fill='#0777C4' height={24} width={24} />}
                 iconPosition='center'
             >
-                <Text size={isDesktop ? 'sm' : 'md'}>
-                    <Localize
-                        components={[
-                            <a
-                                className='font-bold underline cursor-pointer'
-                                key={0}
-                                onClick={() => showModal('FundsModal')}
-                            />,
-                        ]}
-                        i18n_default_text='Your P2P funds are accessible through your Options trading account. <0>Learn more</0>'
-                    />
-                </Text>
+                <div>
+                    <Text className='mr-1' size={textSize}>
+                        <Localize i18n_default_text='Your P2P funds are accessible through your Options trading account.' />
+                    </Text>
+                    <Button
+                        className='funds-banner__inline-message__button'
+                        color='black'
+                        hideHoverStyles
+                        onClick={() => showModal('FundsModal')}
+                        textSize={textSize}
+                        variant='ghost'
+                    >
+                        <Localize i18n_default_text='Learn more' />
+                    </Button>
+                </div>
             </InlineMessage>
             {!!isModalOpenFor('FundsModal') && <FundsModal isModalOpen onRequestClose={hideModal} />}
         </div>
