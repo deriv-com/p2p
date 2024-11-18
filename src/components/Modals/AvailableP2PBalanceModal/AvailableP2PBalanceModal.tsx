b719a191cb7a08b9c0d30b29c0e7a58d4db5c4f6
Farrah Mae Ochoa <82315152+farrah-deriv@users.noreply.github.com>
2024-11-18 11:22:28 +0400

    fix: available balance message (#396)

--- src/components/Modals/AvailableP2PBalanceModal/AvailableP2PBalanceModal.tsx
+++ src/components/Modals/AvailableP2PBalanceModal/AvailableP2PBalanceModal.tsx
@@ -19,28 +19,31 @@ const AvailableP2PBalanceModal = ({ isModalOpen, onRequestClose }: TAvailableP2P
             style={customStyles}
             testId='dt_available_p2p_balance_modal'
         >
-            <Modal.Header className='p-0 h-auto' hideBorder hideCloseIcon>
+            <Modal.Header className='pl-0 h-auto' hideBorder hideCloseIcon>
                 <Text as='p' weight='bold'>
                     <Localize i18n_default_text='Deriv P2P balance' />
                 </Text>
             </Modal.Header>
             <Modal.Body>
                 <Text as='p' className='available-balance-modal__text' size='sm'>
-                    <Localize i18n_default_text='Your Deriv P2P balance includes:' />
+                    <Localize i18n_default_text='Your Deriv P2P balance is made up of:' />
                 </Text>
                 <ol className='available-balance-modal__list'>
                     <Text as='li' size='sm'>
-                        <Localize i18n_default_text='P2P deposits: Funds received from buying USD from another Deriv P2P user.' />
+                        <Localize i18n_default_text='Funds you received from buying USD on Deriv P2P.' />
                     </Text>
                     <Text as='li' size='sm'>
-                        <Localize i18n_default_text='Non-reversible deposits: Deposits from non-reversible payment methods.' />
+                        <Localize i18n_default_text='Profits from your trades, which you can sell to other Deriv P2P users.' />
+                    </Text>
+                    <Text as='li' size='sm'>
+                        <Localize i18n_default_text='Deposits you made through non-reversible payment methods.' />
                     </Text>
                 </ol>
                 <Text as='p' className='available-balance-modal__note' size='sm'>
-                    <Localize i18n_default_text='Note: Funds deposited using reversible payment methods, like credit cards, Maestro, Diners Club, ZingPay, Skrill, Neteller, Ozow, and UPI QR will not appear in your P2P balance.' />
+                    <Localize i18n_default_text='Note: Funds deposited using reversible payment methods, like credit cards, Maestro, Diners Club, ZingPay, Skrill, Neteller, Ozow, and UPI QR, will not appear in your P2P balance.' />
                 </Text>
             </Modal.Body>
-            <Modal.Footer className='p-0 min-h-auto' hideBorder>
+            <Modal.Footer className='p-0 min-h-fit' hideBorder>
                 <Button onClick={onRequestClose} size='lg' textSize='sm'>
                     <Localize i18n_default_text='OK' />
                 </Button>
