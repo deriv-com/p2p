4decb0af0b9c9e54d753aab3f57b4bea0b7a2186
ameerul-deriv <ameerul@deriv.com>
2025-03-13 17:46:17 +0800

    fix: logic to determine if user is using a wallet account

--- src/hooks/custom-hooks/useShouldRedirectToLowCodeHub.ts
+++ src/hooks/custom-hooks/useShouldRedirectToLowCodeHub.ts
@@ -1,5 +1,5 @@
-import { useAuthData } from '@deriv-com/api-hooks';
 import { URLConstants } from '@deriv-com/utils';
+import { api } from '..';
 import useGrowthbookGetFeatureValue from './useGrowthbookGetFeatureValue';
 
 type TUseShouldRedirectToLowCodeHub = (goToCFDs?: boolean) => string;
@@ -7,14 +7,14 @@ type TUseShouldRedirectToLowCodeHub = (goToCFDs?: boolean) => string;
 const useShouldRedirectToLowCodeHub: TUseShouldRedirectToLowCodeHub = (goToCFDs = false) => {
     const isProduction = process.env.NODE_ENV === 'production';
     const isStaging = process.env.NODE_ENV === 'staging';
-    const { data } = useAuthData();
+    const { data: activeAccount } = api.account.useActiveAccount();
     const [hubEnabledCountryListP2P] = useGrowthbookGetFeatureValue({
         featureFlag: 'hub_enabled_country_list_p2p',
     });
 
-    const hasWalletAccount = data?.authorize?.linked_to?.some(item => item.platform === 'dwallet');
+    const hasWalletAccount = activeAccount?.isWalletAccount;
     const isUserCountryInHubEnabledCountryList = Array.isArray(hubEnabledCountryListP2P)
-        ? hubEnabledCountryListP2P.includes(data?.authorize?.country ?? '')
+        ? hubEnabledCountryListP2P.includes(activeAccount?.country ?? '')
         : false;
 
     const shouldRedirectToLowCode = hasWalletAccount && isUserCountryInHubEnabledCountryList;
