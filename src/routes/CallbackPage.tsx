ba6e4e2601407f8492300b2a8bb94f91edf363f8
ameerul-deriv <ameerul@deriv.com>
2025-03-21 16:03:37 +0800

    fix: added logic to return USD token for OIDC

--- src/routes/CallbackPage.tsx
+++ src/routes/CallbackPage.tsx
@@ -1,11 +1,32 @@
 import { memo } from 'react';
 import { Callback } from '@deriv-com/auth-client';
 
+type TTokens = {
+    [key: string]: string | undefined;
+};
+
+const groupTokens = (tokens: TTokens) => {
+    const grouped: { acct?: string; cur?: string; token?: string }[] = [];
+
+    for (let i = 1; i <= 3; i++) {
+        grouped.push({
+            acct: tokens[`acct${i}`],
+            cur: tokens[`cur${i}`],
+            token: tokens[`token${i}`],
+        });
+    }
+
+    return grouped;
+};
+
 const CallbackPage = () => {
     return (
         <Callback
             onSignInSuccess={tokens => {
-                localStorage.setItem('authToken', tokens.token1);
+                const groupedTokens = groupTokens(tokens);
+                const selectedAuthToken = groupedTokens.find(item => item.cur === 'USD')?.token || tokens.token1;
+
+                localStorage.setItem('authToken', selectedAuthToken);
 
                 window.location.href = '/';
             }}
