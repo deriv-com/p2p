4ec17a4d855101c28d7df957925a2df715a5b638
Farrah Mae Ochoa <82315152+farrah-deriv@users.noreply.github.com>
2025-04-30 15:46:53 +0400

    Revert "fix: callback page"

--- src/routes/CallbackPage.tsx
+++ src/routes/CallbackPage.tsx
@@ -33,7 +33,7 @@ const CallbackPage = () => {
 
                 localStorage.setItem('authToken', selectedAuthToken);
 
-                history.replaceState(null, '', '/');
+                window.location.href = '/';
             }}
         />
     );
