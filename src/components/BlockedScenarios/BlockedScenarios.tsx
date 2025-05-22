b97cd9fc4dbc2ceff87aa29e58b76a95521265b6
Farrah Mae Ochoa <farrah@deriv.com>
2025-05-22 16:40:31 +0400

    fix: types

--- src/components/BlockedScenarios/BlockedScenarios.tsx
+++ src/components/BlockedScenarios/BlockedScenarios.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
 import { useEffect, useState } from 'react';
 import { api, useShouldRedirectToLowCodeHub } from '@/hooks';
 import { Chat } from '@/utils';
