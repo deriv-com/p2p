d0c77fbdd2ad706c8c93f968f2a6d80217fe6d1a
Farrah Mae Ochoa <farrah@deriv.com>
2025-05-26 15:15:27 +0400

    fix: url

--- src/hooks/custom-hooks/useGetHubEnabledCountryList.ts
+++ src/hooks/custom-hooks/useGetHubEnabledCountryList.ts
@@ -2,7 +2,7 @@ import { HUB_ENABLED_COUNTRY_LIST } from '@/constants';
 
 /** A custom hook that returns the remote configuration to get hub_enabled_country_list. */
 const useGetHubEnabledCountryList = () => {
-    const HUB_REMOTE_CONFIG_URL = process.env.HUB_REMOTE_CONFIG_URL || '';
+    const HUB_REMOTE_CONFIG_URL = process.env.VITE_HUB_REMOTE_CONFIG_URL || '';
 
     if (HUB_REMOTE_CONFIG_URL) {
         const getRemoteConfig = async () => {
