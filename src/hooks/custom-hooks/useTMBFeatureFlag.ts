f5901fb99391cf6b6a5fd9b13ea701678f41f069
Farrah Mae Ochoa <farrah@deriv.com>
2025-05-27 10:08:58 +0400

    fix: condition

--- src/hooks/custom-hooks/useTMBFeatureFlag.ts
+++ src/hooks/custom-hooks/useTMBFeatureFlag.ts
@@ -1,18 +1,18 @@
 /** A custom hook that returns the value for the tmb feature flag. */
 const useTMBFeatureFlag = () => {
     const TMB_REMOTE_CONFIG_URL = process.env.VITE_TMB_REMOTE_CONFIG_URL || '';
-    const isTMBEnabled = JSON.parse(localStorage.getItem('is_tmb_enabled') ?? 'false');
+    const isTMBEnabled = JSON.parse(localStorage.getItem('is_tmb_enabled') as string);
 
     if (TMB_REMOTE_CONFIG_URL) {
         const getRemoteConfig = async () => {
             const response = await fetch(TMB_REMOTE_CONFIG_URL)
                 .then(res => res.json())
                 .catch(() => {
-                    return false;
+                    return true;
                 });
 
             return {
-                data: response.p2p ?? isTMBEnabled,
+                data: isTMBEnabled ?? response.p2p,
             };
         };
 
