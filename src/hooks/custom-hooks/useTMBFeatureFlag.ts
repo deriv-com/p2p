c6c497f4919d0226d050c04eebc31ff9ada01f0f
Farrah Mae Ochoa <farrah@deriv.com>
2025-05-30 09:22:11 +0400

    fix: enabled tmb

--- src/hooks/custom-hooks/useTMBFeatureFlag.ts
+++ src/hooks/custom-hooks/useTMBFeatureFlag.ts
@@ -4,6 +4,7 @@ const useTMBFeatureFlag = () => {
     const TMB_REMOTE_CONFIG_URL = process.env.VITE_TMB_REMOTE_CONFIG_URL || '';
     const isTMBEnabled = JSON.parse(localStorage.getItem('is_tmb_enabled') as string);
     const [isTMBEnabledValue, setIsTMBEnabledValue] = useState<boolean>(isTMBEnabled);
+    const [isInitialized, setIsInitialized] = useState<boolean>(false);
 
     useEffect(() => {
         if (TMB_REMOTE_CONFIG_URL) {
@@ -14,17 +15,22 @@ const useTMBFeatureFlag = () => {
                     const response = await fetch(TMB_REMOTE_CONFIG_URL);
                     const data = await response.json();
                     setIsTMBEnabledValue(data.p2p);
+                    setIsInitialized(true);
                 } catch (error) {
                     setIsTMBEnabledValue(true);
+                    setIsInitialized(true);
                 }
             };
 
             getRemoteConfig();
+        } else {
+            setIsInitialized(true);
         }
     }, []);
 
     return {
         data: isTMBEnabledValue,
+        isInitialized,
     };
 };
 
